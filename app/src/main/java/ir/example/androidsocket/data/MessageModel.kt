d56a1b11fa5de3ca3d3a1701d608d6922c7532f6
sepideh.akbarinezhad <sepideh.akbarinezhad@eniac-tech.com>
2024-07-09 13:21:17 +0330

    clean project

--- app/src/main/java/ir/example/androidsocket/data/MessageModel.kt
+++
@@ -1,8 +0,0 @@
-package ir.example.androidsocket.data
-
-data class MessageModel<T>(
-    val actionCode: Int? = null,
-    val data: T? = null,
-    val message: String? = null
-)
-
--- app/src/main/java/ir/example/androidsocket/socket/SocketResponse.kt
+++
@@ -1,8 +0,0 @@
-package ir.example.androidsocket.socket
-
-data class SocketResponse<out T> (
-    val action : Boolean? = null,
-    val data: T? = null,
-    val message: String? = null,
-    val code: String? = null,
-)
